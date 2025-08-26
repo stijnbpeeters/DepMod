bd966792a133f1b2f401a108e1fb9271378e8d9e
Stijn Peeters <85304spe@eur.nl>
2025-08-26 10:57:40 +0200

    Package release on GitHub

---
+++ R/run_app.R
@@ -0,0 +1,12 @@
+#' Run the Shiny app
+#'
+#' Launches the Shiny app included in this package.
+#' @export
+run_app <- function(...) {
+  app_dir <- system.file("app", package = utils::packageName())
+  if (!nzchar(app_dir)) {
+    stop("App directory not found. Was the package installed correctly?",
+         call. = FALSE)
+  }
+  shiny::shinyAppDir(appDir = app_dir, ...)
+}
